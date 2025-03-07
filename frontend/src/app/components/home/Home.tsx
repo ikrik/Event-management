ba57bb0f3278d41859fc641da79145dbcd51132d
Krik Tsakma <gregtsakma@gmail.com>
2025-03-07 21:34:52 +0200

    [FRONT-END] Update Home to be the only one that fetches Events (search, pagination)

--- frontend/src/app/components/home/Home.tsx
+++ frontend/src/app/components/home/Home.tsx
@@ -2,16 +2,57 @@
 import DataTable from '@components/dataTable/DataTable';
 import { DynamicDate } from '@components/dynamicDate/DynamicDate';
 import { EventCreateButton } from '@components/eventCreateButton/EventCreateButton';
+import Pager from '@components/pagination/Pagination';
 import RecentEvent from '@components/recentEvent/RecentEvent';
 import { Search } from '@components/search/Search';
-import { ReactNode, useState } from 'react';
-import { EventItem, extraStyles, headers, iconsPerKey } from 'types/events.types';
+import useGetEvents from '@hooks/useGetEvents';
+import { convertObjectToURLParams, isArrayEqualUnordered } from '@utils/helpers';
+import { useEffect, useState } from 'react';
+import { extraStyles, FormattedEventItem, headers, iconsPerKey, Pagination, QueryParams } from 'types/events.types';
 
-export default function Home({ initialEvents }: { initialEvents: (EventItem & Record<string, ReactNode>)[] }) {
-  const [filteredEvents, setFilteredEvents] = useState(initialEvents);
+interface HomeProps {
+  initialEvents: FormattedEventItem[];
+  initPagination: Required<Pagination>;
+}
 
-  const updateEvents = (newEvents: (EventItem & Record<string, ReactNode>)[]) => {
-    setFilteredEvents(newEvents);
+export default function Home({ initialEvents, initPagination }: HomeProps) {
+  const [query, setQuery] = useState<Partial<QueryParams>>({});
+  const [shouldFetch, setShouldFetch] = useState<boolean>(false);
+  const [events, setEvents] = useState(initialEvents);
+  const [pagination, setPagination] = useState(initPagination);
+
+  const {
+    events: dataEvents,
+    pagination: paging,
+    error,
+    loading,
+  } = useGetEvents({
+    query: convertObjectToURLParams(query),
+    shouldFetch,
+  });
+
+  const updateEvents = (qry: Partial<QueryParams>, shouldCall: boolean) => {
+    setQuery((prev) => {
+      if (typeof qry?.searchLocation === 'string') {
+        return qry?.searchLocation ? { ...qry } : {};
+      }
+      return { ...prev, ...qry };
+    });
+    setShouldFetch(() => shouldCall);
+  };
+
+  useEffect(() => {
+    if (dataEvents && !isArrayEqualUnordered<FormattedEventItem>(dataEvents, events, 'id') && shouldFetch) {
+      setEvents(dataEvents);
+    }
+
+    if (paging && JSON.stringify(paging) !== JSON.stringify(pagination) && shouldFetch) {
+      setPagination(paging);
+    }
+  }, [dataEvents, paging]);
+
+  const handleMoveToPage = (page: number) => {
+    updateEvents({ page }, true);
   };
 
   return (
@@ -36,7 +77,22 @@ export default function Home({ initialEvents }: { initialEvents: (EventItem & Re
         </div>
         <div className="flex-1">
           <h3 className="text-2xl text-zinc-400 mb-6">Events</h3>
-          <DataTable data={filteredEvents} headers={headers} extraStyles={extraStyles} iconsPerKey={iconsPerKey} />
+          {error ? (
+            <div className="text-center">
+              <h3 className="text-3xl text-zinc-200 my-10">An error occurred while fetching the Events List</h3>
+            </div>
+          ) : (
+            <>
+              <DataTable
+                loading={shouldFetch && loading}
+                data={events}
+                headers={headers}
+                extraStyles={extraStyles}
+                iconsPerKey={iconsPerKey}
+              />
+              <Pager onMoveToPage={handleMoveToPage} {...pagination} />
+            </>
+          )}
         </div>
       </section>
     </>
