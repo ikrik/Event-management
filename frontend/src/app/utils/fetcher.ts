ded9bb9207adfbe576bf2308f4f98ab0b2ac953e
Krik Tsakma <gregtsakma@gmail.com>
2025-03-10 13:30:09 +0200

    [FRONT-END] Update fetcher to receive all fetch methods

--- frontend/src/app/utils/fetcher.ts
+++ frontend/src/app/utils/fetcher.ts
@@ -1,5 +1,14 @@
-export async function fetcher(url: string) {
-  const res = await fetch(url);
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function fetcher(url: string, method: string = 'GET', body?: any) {
+  const options: RequestInit = {
+    method,
+    cache: 'no-store',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: body ? JSON.stringify(body) : undefined,
+  };
+  const res = await fetch(url, options);
   if (!res.ok) {
     throw new Error(`API request failed with status: ${res.status}`);
   }
