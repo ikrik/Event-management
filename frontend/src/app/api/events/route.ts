275ee85d6dbe6292987464993b58c40fec26c4cc
Krik Tsakma <gregtsakma@gmail.com>
2025-03-10 13:31:04 +0200

    [FRONT-END] Add api POST & PUT methods (from next server to nest)

--- frontend/src/app/api/events/route.ts
+++ frontend/src/app/api/events/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: NextRequest) {
   const params = req.nextUrl.searchParams.toString();
   const queryParams = params?.length > 0 ? `?${params}` : '';
   try {
-    const res = await fetch(`${BASE_API_URL}/events${queryParams}`);
+    const res = await fetch(`${BASE_API_URL}/events${queryParams}`, { cache: 'no-store' });
     if (!res.ok) {
       throw new Error('Failed to fetch data');
     }
@@ -14,5 +14,53 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(events);
   } catch (e) {
     return NextResponse.json({ error: 'Failed to fetch events' }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const res = await fetch(`${BASE_API_URL}/events`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      throw new Error('Failed to Save the data');
+    }
+    const events = await res.json();
+    return NextResponse.json(events);
+  } catch (e) {
+    return NextResponse.json({ error: (e as Error).message }, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  const body = await req.json();
+  const { id, ...restBody } = body;
+
+  if (!id) {
+    return NextResponse.json({ error: 'Missing event ID' }, { status: 400 });
+  }
+
+  try {
+    const res = await fetch(`${BASE_API_URL}/events/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(restBody),
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to Update the data');
+    }
+
+    const updatedEvent = await res.json();
+    return NextResponse.json(updatedEvent);
+  } catch (e) {
+    return NextResponse.json({ error: (e as Error).message }, { status: 500 });
   }
 }
